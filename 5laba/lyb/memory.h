2162c59e83acc770b9c45c929d686c25da85fc95
a9ek0 <109074282+a9ek0@users.noreply.github.com>
2023-05-06 20:29:32 +0300

    Delete 5laba directory

--- 5laba/lyb/memory.h
+++
@@ -1,9 +0,0 @@
-#ifndef INC_5LABA_MEMORY_H
-#define INC_5LABA_MEMORY_H
-#include "cache.h"
-#include "structures.h"
-void free_cache(CACHE *cache);
-void free_node(NODE *node);
-NODE* create_node(char *key, char *value);
-CACHE *create_cache();
-#endif //INC_5LABA_MEMORY_H
